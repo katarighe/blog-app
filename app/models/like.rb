41cf3bd66f17475eb6227b857396ccd52570dc8c
katarighe <m.ighe@hotmail.com>
2023-10-25 16:45:44 +0300

    Update the classes

--- app/models/like.rb
+++ app/models/like.rb
@@ -4,10 +4,10 @@ class Like < ApplicationRecord
   belongs_to :post, class_name: 'Post'
 
   # Callbacks
-  after_save :update_post_likes_counter
+  after_save :update_likes_counter
 
   # Methods
-  def update_post_likes_counter
+  def update_likes_counter
     post.update(likes_counter: post.likes.count)
   end
 end
