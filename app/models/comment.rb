1f1c2286035611897b24b38da7ed86bac53aa880
katarighe <m.ighe@hotmail.com>
2023-10-25 16:31:51 +0300

    Update the Comment class

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,17 +1,11 @@
 class Comment < ApplicationRecord
   # Associations
-  belongs_to :author, class_name: 'User'
-  belongs_to :post, class_name: 'Post'
-
-  # Attributes
-  attribute :text, :text
-
-  # Callbacks
-  after_create :update_post_comments_counter
-  after_destroy :update_post_comments_counter
+  belongs_to :post
+  belongs_to :user
+  after_save :update_comments_counter
 
   # Methods
-  def update_post_comments_counter
-    post.update(comments_counter: post.comments.count)
+  def update_comments_counter
+    post.increment!(:comments_counter)
   end
 end
