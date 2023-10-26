7b030096266e4462ced643979de314ade076afff
katarighe <m.ighe@hotmail.com>
2023-10-26 16:38:37 +0300

    Update the User model in order to make the testing simple.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,8 @@ class User < ApplicationRecord
   has_many :comments
   has_many :likes
 
-  # Attributes
-  attribute :name, :string
-  attribute :bio, :text
-  attribute :posts_counter, :integer, default: 0
-  attribute :photo, :string
-
   # Methods
   def three_most_recent_posts
-    posts.order(created_at: :desc).limit(3)
+    Post.where(author: self).order(created_at: :desc).first(3)
   end
 end
