38e4583546eb7db470087d175c911830491424d4
katarighe <m.ighe@hotmail.com>
2023-10-26 16:50:25 +0300

    Add validations for the User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ApplicationRecord
   # Associations
-  has_many :posts, foreign_key: :author_id
-  has_many :comments
-  has_many :likes
+  has_many :post, foreign_key: 'author_id'
+  has_many :comment
+  has_many :like
 
+  # Validations
+  validates :name, presence: true
+  validates :posts_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  
   # Methods
   def three_most_recent_posts
     Post.where(author: self).order(created_at: :desc).first(3)
