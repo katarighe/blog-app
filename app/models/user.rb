c9ab5a5cb34fc7ec44e8b3e238ae74367440f1f0
katarighe <m.ighe@hotmail.com>
2023-10-26 17:03:04 +0300

    Fix linter errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   # Validations
   validates :name, presence: true
   validates :posts_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  
+
   # Methods
   def three_most_recent_posts
     Post.where(author: self).order(created_at: :desc).first(3)
