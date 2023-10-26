1b48b9a8a2e11ef60d569b6d321866821f1a58de
katarighe <m.ighe@hotmail.com>
2023-10-26 17:04:03 +0300

    Update the DB schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_24_131925) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_26_135741) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,8 +37,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_24_131925) do
     t.bigint "author_id"
     t.string "title"
     t.string "text"
-    t.integer "likes_counter"
-    t.integer "comments_counter"
+    t.integer "likes_counter", default: 0
+    t.integer "comments_counter", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["author_id"], name: "index_posts_on_author_id"
@@ -48,7 +48,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_24_131925) do
     t.string "name"
     t.string "photo"
     t.string "bio"
-    t.integer "posts_counter"
+    t.integer "posts_counter", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
