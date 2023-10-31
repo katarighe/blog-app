f64df2ffd10cfc15c4f0fbb1f9a21d57de89976b
katarighe <m.ighe@hotmail.com>
2023-10-31 11:28:35 +0300

    Correct each of the specs after fixing the routes.rb file

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,51 +1,51 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-    context 'GET /index' do
-        before :each do
-            get '/users/:user_id/posts'
-end
-
-it 'returns a sucessful response' do
-    expect(response).to be_sucessful
-end
-
-it 'returns as http status 200' do
-    expect(response.status).to eq(200)
-end
-
-it 'renders the right view file' do
-    expect(response).to render_template(:index)
-end
-
-it 'renders the right placeholder' do
-    expect(response.body).to include('<h1>In this section, here is a list of the posts shown for a designated user.</h1>')
-end
-end
-
-context 'GET /show' do
-    let(:user) { User.create(name: 'Tom' ) }
-    let(:valid_attributes) { {'author' => user, 'title' => 'Title' } }
-    let(:post) { Post.create! valid_attributes }
-
+  context 'GET /index' do
     before :each do
-        get "/users/:user_id/posts/#{post.id}"
+      get '/users/:user_id/posts'
     end
 
-    it 'returns as a sucessful response' do
-        expect(response).to be_sucessful
+    it 'returns as a successful response' do
+      expect(response).to be_successful
     end
 
     it 'returns as http status 200' do
-        expect(response.status).to eq(200)
+      expect(response.status).to eq(200)
     end
 
     it 'renders as the right view file' do
-        expect(response).to render_template(:show)
+      expect(response).to render_template(:index)
     end
 
     it 'renders as the right placeholder' do
-        expect(response.body).to include('<h1>In this section, here is a selected post with for a designated user.</h1>')
+      expect(response.body).to include('<h1>In this section, here is a list of the posts shown for a designated user.</h1>')
     end
-end
+  end
+
+  context 'GET /show' do
+    let(:user) { User.create(name: 'Lily') }
+    let(:valid_attributes) { { 'author' => user, 'title' => 'Title' } }
+    let(:post) { Post.create! valid_attributes }
+
+    before :each do
+      get "/users/:user_id/posts/#{post.id}"
+    end
+
+    it 'returns as a successful response' do
+      expect(response).to be_successful
+    end
+
+    it 'returns as http status 200' do
+      expect(response.status).to eq(200)
+    end
+
+    it 'renders as the right view file' do
+      expect(response).to render_template(:show)
+    end
+
+    it 'renders as the right placeholder' do
+      expect(response.body).to include('<h1>In this section, here is a selected post with for a designated user.</h1>')
+    end
+  end
 end
