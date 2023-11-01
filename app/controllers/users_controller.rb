a1f491fa8919dd3e2b4d00bb558faa1e4bdc83a7
katarighe <m.ighe@hotmail.com>
2023-11-01 11:28:09 +0300

    Fix linter error on the Users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
-layout 'standard'
-def index
-  @users = User.order(id: :asc)
-end
+  layout 'standard'
+  def index
+    @users = User.order(id: :asc)
+  end
 
-def show
-  @user = User.find(params[:id])
-end
+  def show
+    @user = User.find(params[:id])
+  end
 end
