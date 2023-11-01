de9ccf358352c5398f8d850ab39967cc0aa2e465
katarighe <m.ighe@hotmail.com>
2023-11-01 16:56:41 +0300

    Fix linter errors on the User case

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :post, foreign_key: 'author_id'
   has_many :comments
   has_many :likes
-  
+
   # Validations
   validates :name, presence: true
   validates :posts_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
