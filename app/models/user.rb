1bda00f109ad91a272e4be33112c8cab4347a5ed
katarighe <m.ighe@hotmail.com>
2023-11-01 16:47:03 +0300

    Remove the foreign key on comments and likes part

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
   # Associations
   has_many :post, foreign_key: 'author_id'
-  has_many :comments, foreign_key: 'author_id'
-  has_many :likes, foreign_key: 'author_id'
-
+  has_many :comments
+  has_many :likes
+  
   # Validations
   validates :name, presence: true
   validates :posts_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
