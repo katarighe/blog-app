0e3be98f0ee0c3a0791a303038ef66d8602cd22e
katarighe <m.ighe@hotmail.com>
2023-11-06 12:42:49 +0300

    Fix linter errors from the n+1 changes

--- spec/features/placeholder.rb
+++ spec/features/placeholder.rb
@@ -1,6 +1,6 @@
 module TestPlaceholders
-    def placeholder
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt \
+  def placeholder
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt \
       labore et dolore magna aliqua. Ut porttitor leo a diam sollicitudin tempor id eu nisl. \
       Facilisis mauris sit amet massa vitae. Nulla facilisi etiam dignissim diam quis enim. \
       Pulvinar etiam non quam lacus suspendisse faucibus. Aliquet nec ullamcorper sit amet risus \
@@ -17,7 +17,7 @@ module TestPlaceholders
       faucibus scelerisque eleifend donec pretium vulputate. Sollicitudin aliquam ultrices sagittis \
       orci a scelerisque. Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Dignissim \
       enim sit amet venenatis urna cursus eget.
-      
+
       Sollicitudin aliquam ultrices sagittis orci a scelerisque purus semper. Ipsum nunc aliquet \
       bibendum enim. Ornare lectus sit amet est placerat in egestas erat imperdiet. Suspendisse \
       faucibus interdum posuere lorem ipsum dolor sit amet. Mauris a diam maecenas sed enim ut sem. \
@@ -26,5 +26,5 @@ module TestPlaceholders
       pellentesque massa placerat duis ultricies lacus sed. Tortor condimentum lacinia quis vel eros \
       donec ac odio. Ac turpis egestas integer eget aliquet nibh. Risus nullam eget felis eget nunc \
       lobortis mattis aliquam. Facilisis gravida neque convallis a cras semper auctor neque."
-    end
   end
+end
