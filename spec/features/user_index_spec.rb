aa29fd16ccd53975af3c416438b864109afc7495
katarighe <m.ighe@hotmail.com>
2023-11-06 15:55:51 +0300

    Try to write the code again and fix the failure errors

--- spec/features/user_index_spec.rb
+++ spec/features/user_index_spec.rb
@@ -1,32 +1,34 @@
 require 'rails_helper'
 
-feature 'User index page' do
+RSpec.feature 'User index page', type: :feature do
   scenario 'I can see the username of all other users' do
     visit users_path
-    @users.each do |user|
-      expect(page).to have_content(user.name)
+    expect(page).to have_selector('.user', count: User.count)
+    User.all.each do |user|
+      expect(page).to have_link(user.name, href: user_url(user))
     end
   end
 
   scenario 'I can see the profile picture for each user' do
     visit users_path
-    @users.each do |user|
-      expect(page).to have_css("img[src='#{user.avatar_url}']")
+    User.all.each do |user|
+      expect(page).to have_css("#user_#{user.id} img.profile-picture")
     end
   end
 
   scenario 'I can see the number of posts each user has written' do
     visit users_path
-
-    @users.each do |user|
-      expect(page).to have_content("Posts: #{user.posts_count}")
+    User.all.each do |user|
+      expect(page).to have_content("Posts: #{user.posts.count}")
     end
   end
 
-  scenario 'When I click on a user, I am redirected to that users show page' do
+  scenario 'When I click on a user, I am redirected to that user\'s show page' do
     visit users_path
-    user = @users.first
-    click_link user.name
-    expect(page).to have_current_path(user_path(user))
+    User.all.each do |user|
+      find("#user_#{user.id}").click
+      expect(current_path).to eq(user_path(user))
+      visit users_path
+    end
   end
 end
