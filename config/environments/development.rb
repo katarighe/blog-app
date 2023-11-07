4f708436924a07be43f48d65e00e8da351d69adb
katarighe <m.ighe@hotmail.com>
2023-11-07 14:11:17 +0300

    Add config statements for the email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,8 +8,6 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.enable_reloading = true
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-
   # Do not eager load code on boot.
   config.eager_load = false
 
@@ -42,6 +40,14 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: '127.0.0.1',
+    port: 1025
+  }
+  
+  config.action_mailer.perform_deliveries = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
