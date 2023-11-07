0f68cd41efff37553249c732d25dd3c59d5c457f
katarighe <m.ighe@hotmail.com>
2023-11-07 04:21:44 +0300

    Fix linter error

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
-    factory :user do
-      name { Faker::Name.name }
-      photo { Faker::Internet.url }
-      bio { Faker::Lorem.sentence }
-    end
+  factory :user do
+    name { Faker::Name.name }
+    photo { Faker::Internet.url }
+    bio { Faker::Lorem.sentence }
   end
+end
