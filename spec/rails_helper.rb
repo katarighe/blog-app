f9d241d5b359e8620e2576dd22a7cd5dc65fb6e1
katarighe <m.ighe@hotmail.com>
2023-11-07 04:22:14 +0300

    Fix linter error on the rails

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,7 +8,7 @@ require 'rspec/rails'
 require 'factory_bot'
 require 'capybara/rails'
 
-# Add additional requires below this line. Rails is not loaded until this point!
+# Add additional requires below this line. Rails is not loaded until this point!z
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
