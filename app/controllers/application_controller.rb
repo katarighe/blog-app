198e89cd34a168848426fff8209e9e8ce5469e6e
katarighe <m.ighe@hotmail.com>
2023-11-08 15:56:02 +0300

    Include authorization rules on the controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,19 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :load_and_authorize_resource
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation) }
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
+  end
+
+  private
+
+  def load_and_authorize_resource
+    # Load and authorize resources as needed
   end
 end
